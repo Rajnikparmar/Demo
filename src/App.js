5e3231c5776cb2767c2402711d0b08a9c1df440e
Rajnikparmar <rajnikparmar25@gmail.com>
2021-12-31 22:57:40 +0530

    demo project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './Components/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+// import '.\src\Components\Style.css';
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Home.js
@@ -0,0 +1,209 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        
+            <div className="container mt-3">
+                <div className="card card1 p-2">
+                    <div className="innercard p-2">
+                        <nav className="navbar navbar-expand-lg navbar-light ">
+                            <div className="container-fluid">
+                            <img src="/home.jpg" height="80px" width="80px" /> <a className="navbar-brand name" href="#">Brush  <span className="go">Sharp</span>
+                            </a> <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon" /> </button>
+                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                        <li className="nav-item"> <a className="nav-link active" aria-current="page" href="#"><span className="home">Home +</span></a> </li>
+                                        <li className="nav-item"> <a className="nav-link" href="#">About +</a> </li>
+                                        <li className="nav-item"> <a className="nav-link" href="#">Services +</a> </li>
+                                        <li className="nav-item"> <a className="nav-link" href="#">Blog +</a> </li>
+                                        <li className="nav-item"> <a className="nav-link" href="#">Shop +</a> </li>
+                                        <li className="nav-item"> <a className="nav-link" href="#">Pages +</a> </li>
+                                        <div className="btn btn-white">Book Now </div>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                       <div bgcolor="#0066FF">
+                           <div className="mt-3 d-flex justify-content-center"> <h3> 30 + Years Experiance </h3> </div>
+                           <div className="d-flex justify-content-center"> <p>Lorean lpsum is simple dummy text of the printing and typesetting industry.lorean <br/> ipsum has been the industry standard dummy text ever since the 1500s.when <br/>en unkown printer look gallery of type and scrambled it to make a type<br/>      specimen book.</p> </div>
+                        
+                           </div>
+                        <div className="row mt-3 p-2 g-3 d-flex justify-content-center">
+                            <table class="table table-striped">
+                                <thead>
+
+                                </thead>
+                                <tbody>
+                                    <h4>Job Description</h4>
+                                    <tr>
+                                        <th scope="row">Part Number</th>
+                                        <th scope="row">Part Description</th>
+                                        <th scope="row">Part Quantity</th>
+                                        <th scope="row">Cost</th>
+
+                                    </tr>
+                                    <tr>
+                                        <td>fsd</td>
+                                        <td>fsde</td>
+                                        <td>3</td>
+                                        <td>$274.29</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Labor</th>
+                                        <td></td>
+                                        <th scope="row">Hours</th>
+                                        <th scope="row">Cost</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Test Labor items</td>
+                                        <td></td>
+                                        <td>0</td>
+                                        <td>$0.00</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Other</th>
+                                        <td></td>
+                                        <th scope="row">Other Description</th>
+                                        <th scope="row">Cost</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Other items</td>
+                                        <td></td>
+                                        <td>Other item</td>
+                                        <td>$0.00</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Discount Title</th>
+                                        <td></td>
+                                        <th scope="row">Type</th>
+                                        <th scope="row">Amount</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Membership Discount Of 10%</td>
+                                        <td></td>
+                                        <td>%10 member_discount</td>
+                                        <td>$28.57</td>
+                                    </tr>
+                                    <tr>
+                                        <td>All Parts & Labor %</td>
+                                        <td></td>
+                                        <td>%10 all_part-and-labours</td>
+                                        <td>$28.57</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Travel Surcharge</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>$285.71</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Text On Travel Surcharge</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>$11.43</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tex</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>$21.94</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td>Total</td>
+                                        <td>$296.23</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+
+
+
+                            <footer className="page-footer font-small unique-color-dark">
+                                <div style={{ backgroundColor: '#6351ce' }}>
+                                    <div className="container">
+                                        <div className="row py-4 d-flex align-items-center">
+                                            <div className="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                                                <h3 className="mb-0">25% Off For New Customers </h3><br/>
+                                                <h6>Loream ipsum dolor sit amet consectetur adipisicing edit</h6>
+                                            </div>
+                                            <div className="col-md-6 col-lg-7 text-center text-md-right">
+                                                <a className="fb-ic">
+                                                    <i className="fab fa-facebook-f white-text mr-4"> </i>
+                                                </a>
+                                                <a className="tw-ic">
+                                                    <i className="fab fa-twitter white-text mr-4"> </i>
+                                                </a>
+                                                <a className="gplus-ic">
+                                                    <i className="fab fa-google-plus-g white-text mr-4"> </i>
+                                                </a>
+                                                <a className="li-ic">
+                                                    <i className="fab fa-linkedin-in white-text mr-4"> </i>
+                                                </a>
+                                                <a className="ins-ic">
+                                                    <i className="fab fa-instagram white-text"> </i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="container text-center text-md-left mt-5">
+                                    <div className="row mt-3">
+                                        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                                        <div className="container-fluid"><img src="/home.jpg" height="80px" width="80px" /> <a className="navbar-brand name" href="#"/>Brush  <span className="go">Sharp</span></div>
+                                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: 60 }} />
+                                            <p>Lorem ipsum dolor sit amet consecteture adipi sicing edit, sed do eiusmod temor incididunt ut labore et dolore magua aliqua dolor.</p>
+                                        </div>
+                                        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                                            <h6 className="text-uppercase font-weight-bold">Quick links</h6>
+                                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: 60 }} />
+                                            <p>
+                                                <h6> About Us</h6>
+                                            </p>
+                                            <p>
+                                                <h6> our Services</h6> 
+                                            </p>
+                                            <p>
+                                            <h6>How It Works </h6> 
+                                            </p>
+                                            
+                                        </div>
+                                        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                            <h6 className="text-uppercase font-weight-bold">Our Services</h6>
+                                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: 60 }} />
+                                            <p>
+                                                 <h6> House Clieaning</h6>
+                                            </p>
+                                            <p>
+                                                 <h6> Office Cleaning</h6>
+                                            </p>
+                                            <p>
+                                                 <h6> After Party Cleaning</h6>
+                                            </p>
+                                            
+                                        </div>
+                                        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                                        <div className="btn btn-success">Book Now </div><br/><br/>
+                                        <div className="btn btn-success">Join Us</div><br/><br/>
+                                        <div className="btn btn-success">NewSletter</div><br/>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </footer>
+
+
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        
+    )
+}
+
+export default Home
